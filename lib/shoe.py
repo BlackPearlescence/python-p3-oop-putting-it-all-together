72147cd2fe20910a38272dff4ea5ff6de53cbdd1
Sani Rogers <sanirogers138@gmail.com>
2023-02-21 13:27:18 -0500

    do putting it all together oop

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self,brand,color = "",size = "", condition = ""):
+        self.brand = brand
+        self.color = color
+        self._size = size
+        self.condition = condition
+    
+    def set_color(self,color):
+        self._color = color
+    def get_color(self):
+        return self._color
+    def set_size(self,size):
+        if(type(size) is not int):
+            print("size must be an integer")
+        else:
+            self._size = size
+    def get_size(self):
+        return self._size
+    
+    def set_brand(self,brand):
+        self.brand = brand
+    def get_brand(self):
+        return self.brand
+
+    def set_condition(self,condition):
+        self.condition = condition
+    def get_condition(self):
+        return self.condition
+
+    def cobble(self):
+        self.set_condition("New")
+        print("Your shoe is as good as new!")
+        
+    size = property(get_size, set_size,)
